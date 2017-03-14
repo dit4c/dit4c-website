d369296a668a3c5e80a7155633c72f75e91d869e
Tim Dettrick <t.dettrick@uq.edu.au>
2017-03-14 15:36:42 +1000

    Use bower not CDN

--- common.js
+++ common.js
@@ -1,6 +1,9 @@
 /* global: module */
 var Metalsmith = require('metalsmith'),
+    _ = require('lodash'),
+    Q = require('q'),
     assets = require('metalsmith-assets'),
+    bower = require('bower'),
     include = require('metalsmith-include-content'),
     layouts = require('metalsmith-layouts'),
     less = require('metalsmith-less'),
@@ -12,6 +15,35 @@ var metadata = {
   "title": "DIT4C",
   "description": "Data Intensive Tools for the Cloud"
 };
+
+var bowerPlugin = function (files, metalsmith, done) {
+  bower.commands
+    .install()
+    .on('end', function () {
+      bower.commands
+        .list()
+        .on('end', function (output) {
+          var libs = (function(data) {
+            var deps = function(m, data) {
+              return _.reduce(data.dependencies, function(m, v, k) {
+                return deps(_.set(m, k, v.canonicalDir), v);
+              }, m);
+            };
+            return deps({}, data);
+          })(output);
+          _.reduce(libs, function(m, v, k) {
+            return m.then(function() {
+              var d = Q.defer();
+              (assets({
+                source: v,
+                destination: './libs/'+k
+              }))(files, metalsmith, function() { d.resolve(); });
+              return d.promise;
+            });
+          }, Q('')).done(function() { done(); });
+        });
+    });
+}
 
 module.exports =
   Metalsmith(__dirname)
@@ -32,4 +64,5 @@ module.exports =
       source: './assets',
       destination: '.'
     }))
+    .use(bowerPlugin)
     .use(wordcount());
